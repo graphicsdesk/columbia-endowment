9f3f31ec4302986c33891d8f29165ef4200aa148
raeedah <raeedah.w@gmail.com>
2020-04-12 05:26:20 -0400

    cleaned

---
+++ endowment-allocations.R
@@ -0,0 +1,57 @@
+## Endowment Values
+library(ggplot2)
+library(hrbrthemes)
+library(xlsx)
+
+balance <- read.xlsx("data/endowment.xlsx",
+                     sheetName = "endowmentBalance",
+                     rowIndex = 1:14,
+                     colIndex = 1:2)
+
+ggplot(balance, aes(x=Financial.Year, y=Endowment.Value, group = 1)) +
+  geom_line() + geom_point() + ggtitle("Columbia University Endowment") + 
+  scale_y_continuous(labels=dollar_format(prefix="$"))
+
+library(treemap)
+
+# Create data
+group <- c("group-1","group-2","group-3")
+value <- c(13,5,22)
+data <- data.frame(group,value)
+
+# treemap
+treemap(data,
+        index="group",
+        vSize="value",
+        type="index"
+)
+
+breakdown <- read.xlsx("data/endowment.xlsx", 
+                       sheetName = "assetAllocations",
+                       rowIndex = 1:16,
+                       colIndex = 1:7)
+### FY 2009
+
+assets2009 <- read.xlsx("data/endowment.xlsx", 
+                        sheetName = "assetAllocations",
+                        rowIndex = 2:8,
+                        colIndex = 13:14)
+treemap(assets2009,
+        index="Group",
+        vSize="allocation",
+        type="index", title = "Columbia 2009 Asset Allocation"
+)
+
+### FY 2019
+
+assets2019 <- read.xlsx("data/endowment.xlsx", 
+                        sheetName = "assetAllocations",
+                        rowIndex = 2:8,
+                        colIndex = 10:11)
+
+# treemap
+treemap(assets2019,
+        index="Group",
+        vSize="allocation",
+        type="index", title = "Columbia 2019 Asset Allocation"
+)
